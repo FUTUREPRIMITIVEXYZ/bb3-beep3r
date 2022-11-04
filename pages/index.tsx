761541d1baf316f73cdd6c620c9d2849999fbfa0
druzydowns <druzydowns@gmail.com>
2022-11-04 16:52:20 -0700

    add text copy and phone number

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,9 +12,13 @@ export default function Home() {
       </Head>
 
       <main className="w-full h-screen flex items-center justify-center overflow-hidden bg-black text-bpr-green relative">
-        <p className="absolute top-0 p-4 bg-black backdrop-blur-lg text-xl uppercase font-mono tracking-wider animate-pulse">
-          beeper coming soon. Still hacking...
-        </p>
+        <a href="sms:+12058838339">
+          <div className="absolute top-0 w-full text-center p-4 cursor-pointer bg-black backdrop-blur-lg text-xl uppercase font-mono tracking-wider">
+            <p>Text me. Do it.</p>
+            <p>+1 205-883-8339</p>
+          </div>
+        </a>
+
         <video
           className="w-full"
           src="/beeper_promo.mp4"
