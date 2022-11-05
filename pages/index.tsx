9fd9d0ea430391ab2236bf1412585380b297596a
Ray <huynh.r.k@gmail.com>
2022-11-04 19:14:01 -0700

    Add authenticated route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,8 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import { getSession } from "next-auth/react";
-import { getToken } from "next-auth/jwt";
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getSession(context);
-  const token = await getToken({ req: context.req });
-
-  const address = token?.sub ?? null;
-  // If you have a value for "address" here, your
-  // server knows the user is authenticated.
-
-  // You can then pass any data you want
-  // to the page component here.
-  return {
-    props: {
-      address,
-      session,
-    },
-  };
-};
-
-type AuthenticatedPageProps = InferGetServerSidePropsType<
-  typeof getServerSideProps
->;
-
-export default function Home({ address }: AuthenticatedPageProps) {
-  console.log({ address });
+export default function Home() {
   return (
     <div>
       <Head>
@@ -54,14 +26,6 @@ export default function Home({ address }: AuthenticatedPageProps) {
           playsInline
         />
       </main>
-      <footer>
-        {address ? (
-          <h1>Authenticated as {address}</h1>
-        ) : (
-          <h1>Unauthenticated</h1>
-        )}
-        <ConnectButton />
-      </footer>
     </div>
   );
 }
