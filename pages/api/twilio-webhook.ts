b6e8550a913285440f5edb13eb6585d55f1a1792
Jayden Windle <jaydenwindle@gmail.com>
2022-11-04 17:03:32 -0700

    Added wallet capture to sms bot (#4)

--- pages/api/twilio-webhook.ts
+++ pages/api/twilio-webhook.ts
@@ -2,6 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import Twilio from "twilio";
 import { PrismaClient } from "@prisma/client";
+import { ethers } from "ethers";
 
 const prisma = new PrismaClient();
 
@@ -10,10 +11,43 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const response = new Twilio.twiml.MessagingResponse();
+  const provider = new ethers.providers.AlchemyProvider(
+    "homestead",
+    process.env.ALCHEMY_API_KEY
+  );
 
   console.log(req.body.Body);
 
+  const body: string = req.body.Body;
   const phone = req.body.From;
+
+  const includedWallet = body
+    .split(" ")
+    .filter((token) => token.includes("0x"))
+    .map((address) => {
+      try {
+        return ethers.utils.getAddress(address);
+      } catch (e) {
+        return null;
+      }
+    })
+    .filter((address) => address !== null)
+    .at(0);
+
+  const walletFromENS = await body
+    .split(" ")
+    .filter((token) => token.includes(".eth"))
+    .map(async (ens) => {
+      try {
+        return await provider.resolveName(ens);
+      } catch (e) {
+        return null;
+      }
+    })
+    .filter((address) => address !== null)
+    .at(0);
+
+  const wallet = includedWallet || walletFromENS;
 
   await prisma.user.upsert({
     where: {
@@ -27,9 +61,24 @@ export default async function handler(
     },
   });
 
-  response.message(
-    "🟢 Message received. Further instructions will be broadcasted before 20:33:000"
-  );
+  if (wallet) {
+    await prisma.user.update({
+      where: {
+        phone,
+      },
+      data: {
+        wallet,
+      },
+    });
+
+    response.message(
+      `🟢 Wallet received. Further instructions will be broadcasted before 20:33:000`
+    );
+  } else {
+    response.message(
+      `🟢 Message received. Further instructions will be broadcasted before 20:33:000`
+    );
+  }
 
   res.setHeader("Content-Type", "text/xml");
 
