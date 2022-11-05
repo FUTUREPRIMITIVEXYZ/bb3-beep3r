34f9876e7c7ad829dd1992cd3b0caf11c2eadbdf
Raymond Huynh <huynh.r.k@gmail.com>
2022-11-04 20:18:10 -0700

    Update schema with code & verify (#9)

---
+++ prisma/migrations/20221105030811_add_code/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[code]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "code" TEXT,
+ADD COLUMN     "verified" BOOLEAN;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_code_key" ON "User"("code");
