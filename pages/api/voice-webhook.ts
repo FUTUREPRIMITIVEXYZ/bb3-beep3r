034b9368271b345d256a080c00ec83a15a2862a0
Jayden Windle <jaydenwindle@gmail.com>
2022-11-05 11:13:35 -0700

    added voice endpoint

---
+++ pages/api/voice-webhook.ts
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import Twilio from "twilio";
+import { PrismaClient } from "@prisma/client";
+import { ethers } from "ethers";
+import crypto from "crypto";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const isValidRequest = Twilio.validateRequest(
+    process.env.TWILIO_AUTH_TOKEN!,
+    req.headers["x-twilio-signature"] as string,
+    process.env.TWILIO_VOICE_WEBHOOK_URL!,
+    req.body
+  );
+
+  if (!isValidRequest) {
+    res.setHeader("Content-Type", "text/xml");
+    res.send(403);
+    return;
+  }
+
+  const response = new Twilio.twiml.VoiceResponse();
+
+  response.say(
+    "greetings ETH SF hacker. this is mizuna from the BB3 mainframe. you have been selected to participate in the beep3r experiment. Text your wallet address or ENS to this number to begin. Goodbye."
+  );
+
+  res.setHeader("Content-Type", "text/xml");
+
+  res.status(200).send(response.toString());
+}
