59dcb85f164bd380ce5b8f39c81474aa3f66936e
Ray <huynh.r.k@gmail.com>
2022-11-06 02:10:46 -0800

    strip out next auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,10 @@ const PUBLIC_PROVIDER = publicProvider();
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const [message, setMessage] = useState("Sign into beeper");
+  const [chains, setChains] = useState<any>({});
+  const [provider, setProvider] = useState<any>({});
+  const [connectors, setConnectors] = useState<any>({});
+  const [wagmiClient, setWagmiClient] = useState<any>({});
   const [initialized, setInitialized] = useState(false);
 
   if (router.pathname.split("/")[1] == "animation") {
@@ -23,21 +27,29 @@ export default function App({ Component, pageProps }: AppProps) {
     );
   }
 
-  const { chains, provider } = configureChains(
-    [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
-    [ALCHEMY_PROVIDER, PUBLIC_PROVIDER]
-  );
+  useEffect(() => {
+    const { chains, provider } = configureChains(
+      [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+      [ALCHEMY_PROVIDER, PUBLIC_PROVIDER]
+    );
 
-  const { connectors } = getDefaultWallets({
-    appName: "My RainbowKit App",
-    chains,
-  });
+    const { connectors } = getDefaultWallets({
+      appName: "My RainbowKit App",
+      chains,
+    });
 
-  const wagmiClient = createClient({
-    autoConnect: true,
-    connectors,
-    provider,
-  });
+    const wagmiClient = createClient({
+      autoConnect: true,
+      connectors,
+      provider,
+    });
+
+    setChains(chains);
+    setProvider(provider);
+    setConnectors(connectors);
+    setWagmiClient(wagmiClient);
+    setInitialized(true);
+  }, []);
 
   if (!initialized) {
     return <div>loading...</div>;
