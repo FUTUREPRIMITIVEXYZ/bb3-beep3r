ffba54dd6d36e0f948d44ebabdd3e3ec530ab463
druzydowns <druzydowns@gmail.com>
2022-11-06 01:05:52 -0700

    style pages, hotfix ui

--- components/menu.tsx
+++ components/menu.tsx
@@ -4,21 +4,25 @@ import { useState } from "react";
 /* eslint-disable @next/next/no-img-element */
 const Menu = ({ ...props }) => {
   const navItems = [
-    { path: "/", icon: "speaker", title: "announcements", id: 1 },
-    { path: "/", icon: "mail", title: "send", id: 2 },
-    { path: "/", icon: "smile", title: "lucky", id: 3 },
-    { path: "/", icon: "web", title: "my messages", id: 4 },
+    { path: "/", icon: "speaker", title: "announcements", id: 1, active: true },
+    { path: "/", icon: "mail", title: "send", id: 2, active: true },
+    { path: "/", icon: "smile", title: "lucky", id: 1, active: false },
+    { path: "/", icon: "web", title: "my messages", id: 1, active: false },
   ];
 
   return (
-    <nav className="bg-greydark flex items-center justify-between p-4 rounded-lg">
+    <nav className="max-w-[500px] bg-greydark flex items-center justify-between p-4 rounded-lg">
       {navItems.map((item: any, i: number) => (
         <Link
           key={i}
-          href={item.path}
+          href={`${item.active ? item.path : "/"}`}
           onClick={() => props.setModalSelection(item.id)}
         >
-          <div className="bg-greylight/10 w-20 h-20 p-2 flex flex-col justify-center items-center rounded-lg cursor-pointer relative">
+          <div
+            className={`bg-greylight/10 w-20 h-20 p-2 flex flex-col justify-center items-center rounded-lg cursor-pointer relative ${
+              item.active ? "" : "opacity-25 pointer-events-none"
+            }`}
+          >
             <img src={`/${item.icon}.png`} alt="" className="w-full h-full" />
             <p className="absolute -bottom-4 text-white text-xs pb-2">
               {item.title}
