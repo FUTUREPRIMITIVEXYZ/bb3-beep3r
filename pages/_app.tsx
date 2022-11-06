2b0112428de11a43d1712f79e6c58ca9d094fe78
Ray <huynh.r.k@gmail.com>
2022-11-06 02:18:00 -0800

    move useEffect above conditional

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,36 +20,37 @@ export default function App({ Component, pageProps }: AppProps) {
   const [connectors, setConnectors] = useState<any>({});
   const [wagmiClient, setWagmiClient] = useState<any>({});
   const [initialized, setInitialized] = useState(false);
+  useEffect(() => {
+    if (router.pathname.split("/")[1] !== "animation") {
+      const { chains, provider } = configureChains(
+        [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+        [ALCHEMY_PROVIDER, PUBLIC_PROVIDER]
+      );
+
+      const { connectors } = getDefaultWallets({
+        appName: "My RainbowKit App",
+        chains,
+      });
+
+      const wagmiClient = createClient({
+        autoConnect: true,
+        connectors,
+        provider,
+      });
+
+      setChains(chains);
+      setProvider(provider);
+      setConnectors(connectors);
+      setWagmiClient(wagmiClient);
+      setInitialized(true);
+    }
+  }, [router.pathname.split("/")[1]]);
 
   if (router.pathname.split("/")[1] == "animation") {
     return (
       <Component {...pageProps} message={message} setMessage={setMessage} />
     );
   }
-
-  useEffect(() => {
-    const { chains, provider } = configureChains(
-      [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
-      [ALCHEMY_PROVIDER, PUBLIC_PROVIDER]
-    );
-
-    const { connectors } = getDefaultWallets({
-      appName: "My RainbowKit App",
-      chains,
-    });
-
-    const wagmiClient = createClient({
-      autoConnect: true,
-      connectors,
-      provider,
-    });
-
-    setChains(chains);
-    setProvider(provider);
-    setConnectors(connectors);
-    setWagmiClient(wagmiClient);
-    setInitialized(true);
-  }, []);
 
   if (!initialized) {
     return <div>loading...</div>;
