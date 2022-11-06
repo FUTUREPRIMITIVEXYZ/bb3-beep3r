b6462c7dbbde74922ee973b6885eda5f8187d7cf
Ray <huynh.r.k@gmail.com>
2022-11-05 20:03:25 -0700

    update hardcoded chain to env

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,7 +11,7 @@ const BEEPER_ADDRESS = process.env.BEEPER_ADDRESS || "";
 const prisma = new PrismaClient();
 
 const provider = new ethers.providers.AlchemyProvider(
-  "homestead",
+  process.env.CHAIN,
   process.env.ALCHEMY_API_KEY
 );
 
